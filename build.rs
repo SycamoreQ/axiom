f423a733933e38f41812522b8aaf3b2545734aef
SycamoreQ <kaushik.80405@gmail.com>
2026-04-26 17:54:17 +0530

    feat(cudarc): fixed cudarc version mismatch

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn cu_sources() -> Vec<PathBuf> {
-    let entries: Vec<PathBuf> = std::fs::read_dir("kernels/src")
+    let entries: Vec<PathBuf> = std::fs::read_dir("kernel/src")
         .unwrap()
         .filter_map(|res| res.ok())
         .filter(|entry| entry.path().is_file())
@@ -19,7 +19,6 @@ fn nvcc_path() -> PathBuf {
     } else {
         PathBuf::from("nvcc")
     }
-    Ok(val)
 }
 
 fn main() {
@@ -28,22 +27,13 @@ fn main() {
     }
     let out_dir = env::var("OUT_DIR").unwrap();
     let ptx_path = PathBuf::from(&out_dir).join("axiom_kernels.ptx");
-    let sources = vec![
-        "kernels/src/fused_residual_rms_[norm.cu](http://norm.cu)",
-        "kernels/src/flash_attention_[4.cu](http://4.cu)",
-        "kernels/src/residual_[attention.cu](http://attention.cu)",
-        "kernels/src/fused_[attention.cu](http://attention.cu)"
-        "kernels/src/argmax_[f16.cu](http://f16.cu)"
-        "kernels/src/copy_blocks_[f16.cu](http://f16.cu)"
-        "kernels/src/flash_attention_[3.cu](http://3.cu)"
-        "kernels/src/rotary_embedding_[f16.cu](http://f16.cu)"
-        "kernels/src/rms_[norm.cu](http://norm.cu)"
-        "kernels/src/reshape_and_cache_[f16.cu](http://f16.cu)"
-    ];
-    let mut command = Command::new("nvcc");
+    let sources = cu_sources();
+    let arch = env::var("CUDA_ARCH").unwrap_or_else(|_| "sm_80".to_string());
+    let mut command = Command::new(nvcc_path);
     command
         .arg("-ptx")
-        .arg("-arch=sm_89")
+        .arg(format!("-arch={}", arch))
+        .arg("-code=sm_80,sm_89,sm_90,sm_100")
         .arg("--expt-relaxed-constexpr")
         .arg("-o")
         .arg(&ptx_path);
